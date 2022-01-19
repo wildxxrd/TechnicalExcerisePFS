961b38187cb7c68b2a176e015ba5a6235b094d31
wilmer <wilmer_will@outlook.com>
2022-01-18 20:05:25 -0500

    -Started Project

---
+++ src/main/java/View/HelloApplication.java
@@ -0,0 +1,26 @@
+package View;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        Label label = new Label("Hello ");
+        label.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(label, 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.example.technicalexercise {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.technicalexercise to javafx.fxml;
+    exports View;
+    opens View to javafx.fxml;
+    exports Controller;
+    opens Controller to javafx.fxml;
+}
